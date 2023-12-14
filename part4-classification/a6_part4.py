fa2e859282faf4301ad90259fb957cc7463cd3f7
Jerry Yant <jyantaratan@cps.edu>
2023-12-14 11:00:06 -0600

    A4 code finished?

--- part4-classification/a6_part4.py
+++ part4-classification/a6_part4.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -10,18 +11,30 @@ x = data[["Age", "EstimatedSalary", "Gender"]].values
 y = data["Purchased"].values
 
 # Step 1: Print the values for x and y
-
+print(x)
+print(y)
 # Step 2: Standardize the data using StandardScaler, 
-
+scaler = StandardScaler().fit(x)
 # Step 3: Transform the data
-
+x = scaler.transform(x)
 # Step 4: Split the data into training and testing data
-
+xtrain, xtest, ytrain, ytest, = train_test_split(x, y, test_size= .2)
 # Step 5: Fit the data
 
 # Step 6: Create a LogsiticRegression object and fit the data
-
+model = linear_model.LogisticRegression().fit(xtrain, ytrain)
+model.fit(xtrain, ytrain)
 # Step 7: Print the score to see the accuracy of the model
+print("The accuracy is ", model.score(xtest, ytest))
+print("-----------")
+print("Test results")
+print("")
 
 # Step 8: Print out the actual ytest values and predicted y values
 # based on the xtest data
+print("Predicted y-value: " + str(model.predict(xtest)) + "Actual y-value: " + str(ytest))
+
+newVar = [["34", "56000", "1"]]
+newVar = scaler.transform(newVar)
+prediction = model.predict(newVar)
+print(prediction)
--- part4-classification/a6_part4_writeup.md
+++ part4-classification/a6_part4_writeup.md
@@ -5,10 +5,10 @@ After completing `a6_part4.py` answer the following questions
 ## Questions to answer
 
 1. Comment out the StandardScaler and re-run your test. How accurate is the model? Why is that?
-
+?
 2. How accurate is the model with the StandardScaler? Is this model accurate enough for the given use case? Explain.
-
+0.9125, yes as from most of the data the predicted values matched 90% of the values
 3. Looking at the predicted and actual results, how did the model do? Was there a pattern to the inputs that the model was incorrect about?
 
 4. Would a 34 year old Female who makes 56000 a year buy an SUV according to the model? Remember to scale the data before running it through the model.
-
+No as the returned value is 0 meaning that there is no correlation
